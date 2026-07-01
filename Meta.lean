1a810bce9174ad48515eeeffd152fe6d6529bdd2
JohnDoe-collab-stack <frederick.loubli@outlook.com>
2026-07-01 13:24:52 +0200

    Revert "Close Collatz diagonal reinsertion fold"
    This reverts commit 2642a85fd4a5f2cf9b8d0a301fc64ad031203b76.

--- Meta.lean
+++ Meta.lean
@@ -29,12 +29,6 @@ import Meta.Collatz.Countdown
 import Meta.Collatz.CountdownConsumptionBridge
 import Meta.Collatz.DynamicClosureLoop
 import Meta.Collatz.DiagonalOrder
-import Meta.Collatz.FibrewiseFlightHeight
-import Meta.Collatz.InitialIndexedFibre
-import Meta.Collatz.OperationalRegimeEvidence
-import Meta.Collatz.OperationalTemporalRepresentation
-import Meta.Collatz.EnrichedTemporalState
-import Meta.Collatz.EnrichedTemporalFibrewiseConcordance
 import Meta.Dynamics.ObservedDiscrete
 import Meta.Dynamics.ObservedWindow
 import Meta.Dynamics.ObservedDynamicGap
@@ -79,11 +73,4 @@ import Meta.Synthesis.TarskiBethBellGap
 #print axioms Meta.EnrichedNatClosedStabilityInstance.collatzIntersectionDiagonalGapOrder_mutual_formedPositiveExcess_eq
 #print axioms Meta.EnrichedNatClosedStabilityInstance.collatzIntersectionDiagonalGap_consumed_as_terminal_excess
 #print axioms Meta.EnrichedNatClosedStabilityInstance.collatzIntersectionDiagonalGap_reenters_as_closing
-#print axioms Meta.EnrichedNatClosedStabilityInstance.collatzInitialIndexFibreHeight
-#print axioms Meta.EnrichedNatClosedStabilityInstance.collatzInitialIndexFibreHeightWitness
-#print axioms Meta.EnrichedNatClosedStabilityInstance.collatzInitialIndexFibreHeight_eq_countdownTerminalExcess
-#print axioms Meta.EnrichedNatClosedStabilityInstance.collatzInitialIndexedFibreHeightPackage
-#print axioms Meta.EnrichedNatClosedStabilityInstance.collatzOperationalTemporalRepresentation
-#print axioms Meta.EnrichedNatClosedStabilityInstance.collatzInitialTemporalFibreState
-#print axioms Meta.EnrichedNatClosedStabilityInstance.collatzEnrichedTemporalHeightConcordance
 /- AXIOM_AUDIT_END -/
